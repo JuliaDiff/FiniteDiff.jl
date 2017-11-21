99f324c2c27737a665d8b3a9b70a80fb8db95c76
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2017-11-20 21:24:31 -0800

    fix diffeqwrapper derivatives

--- src/diffeqwrappers.jl
+++ src/diffeqwrappers.jl
@@ -11,7 +11,6 @@ end
 function finite_difference_jacobian!(J::AbstractMatrix{<:Real}, f, x::AbstractArray{<:Real},
     fdtype::DataType, ::Type{Val{:Real}}, ::Type{Val{:JacobianWrapper}},
     fx::AbstractArray{<:Real}, epsilon::Union{Void,AbstractArray{<:Real}}=nothing, return_type::DataType=eltype(x))
-
     m, n = size(J)
     epsilon_elemtype = compute_epsilon_elemtype(epsilon, x)
     x1, fx1 = f.x1, f.fx1
@@ -21,31 +20,38 @@ function finite_difference_jacobian!(J::AbstractMatrix{<:Real}, f, x::AbstractAr
         epsilon_factor = compute_epsilon_factor(Val{:forward}, epsilon_elemtype)
         @inbounds for i ∈ 1:n
             epsilon = compute_epsilon(Val{:forward}, x[i], epsilon_factor)
+            x1_save = x1[i]
             x1[i] += epsilon
             f(fx1, x1)
             f(fx, x)
-            @. J[:,i] = (vfx1 - vfx) / epsilon
-            x1[i] -= epsilon
+            @. J[:,i] = (vfx - vfx1) / epsilon
+            x1[i] = x1_save
         end
     elseif fdtype == Val{:central}
         epsilon_factor = compute_epsilon_factor(Val{:central}, epsilon_elemtype)
         @inbounds for i ∈ 1:n
             epsilon = compute_epsilon(Val{:central}, x[i], epsilon_factor)
+            x1_save = x1[i]
+            x_save = x[i]
             x1[i] += epsilon
             x[i] -= epsilon
             f(fx1, x1)
             f(fx, x)
-            @. J[:,i] = (vfx1 - vfx) / (2*epsilon)
-            x1[i] -= epsilon
-            x[i] += epsilon
+            @. J[:,i] = (vfx - vfx1) / (2*epsilon)
+            x1[i] = x1_save
+            x[i] = x_save
         end
     elseif fdtype == Val{:complex}
-        x0 = Complex{eltype(x)}(x)
+        x0 = Complex{eltype(x)}.(x)
+        cfx1 = Complex{eltype(x)}.(fx1)
+        vcfx1 = vec(cfx1)
         epsilon = eps(eltype(x))
         @inbounds for i ∈ 1:n
+            x0_save = x0[i]
             x0[i] += im * epsilon
-            @. J[:,i] = imag(f(x0)) / epsilon
-            x0[i] -= im * epsilon
+            f(cfx1,x0)
+            @. J[:,i] = imag(vcfx1) / epsilon # Fix allocation
+            x0[i] = x0_save
         end
     else
         fdtype_error(Val{:Real})
