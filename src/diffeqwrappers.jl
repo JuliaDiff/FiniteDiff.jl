6e3a5f0799e85f8394f4eb2a8cecf053fe3636bc
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2017-11-20 21:45:41 -0800

    make complex wrappers match real

--- src/diffeqwrappers.jl
+++ src/diffeqwrappers.jl
@@ -2,8 +2,6 @@ function finite_difference!(df::AbstractArray{<:Number}, f, x::Union{Number,Abst
     fdtype::DataType, funtype::DataType, ::Type{Val{:DiffEqDerivativeWrapper}},
     fx::Union{Void,AbstractArray{<:Number}}=nothing, epsilon::Union{Void,AbstractArray{<:Real}}=nothing, return_type::DataType=eltype(x))
 
-    # TODO: optimized implementations for specific wrappers using the added DiffEq caching where appopriate
-
     finite_difference!(df, f, x, fdtype, funtype, Val{:Default}, fx, epsilon, return_type)
     df
 end
@@ -62,8 +60,6 @@ end
 function finite_difference_jacobian!(J::AbstractMatrix{<:Number}, f, x::AbstractArray{<:Number},
     fdtype::DataType, ::Type{Val{:Complex}}, ::Type{Val{:JacobianWrapper}},
     fx::AbstractArray{<:Number}, epsilon::Union{Void,AbstractArray{<:Real}}=nothing, return_type::DataType=eltype(x))
-
-    # TODO: test this
     m, n = size(J)
     epsilon_elemtype = compute_epsilon_elemtype(epsilon, x)
     x1, fx1 = f.x1, f.fx1
@@ -73,23 +69,26 @@ function finite_difference_jacobian!(J::AbstractMatrix{<:Number}, f, x::Abstract
         epsilon_factor = compute_epsilon_factor(Val{:forward}, epsilon_elemtype)
         @inbounds for i ∈ 1:n
             epsilon = compute_epsilon(Val{:forward}, real(x[i]), epsilon_factor)
+            x1_save = x1[i]
             x1[i] += epsilon
             f(fx1, x1)
             f(fx, x)
-            @. J[:,i] = ( real( (vfx1 - vfx) ) + im*imag( (vfx1 - vfx) ) ) / epsilon
-            x1[i] -= epsilon
+            @. J[:,i] = ( real( (vfx - vfx1) ) + im*imag( (vfx - vfx1) ) ) / epsilon
+            x1[i] = x1_save
         end
     elseif fdtype == Val{:central}
         epsilon_factor = compute_epsilon_factor(Val{:central}, epsilon_elemtype)
         @inbounds for i ∈ 1:n
             epsilon = compute_epsilon(Val{:central}, real(x[i]), epsilon_factor)
+            x1_save = x1[i]
+            x_save = x[i]
             x1[i] += epsilon
             x[i] -= epsilon
             f(fx1, x1)
             f(fx, x)
-            @. J[:,i] = ( real( (vfx1 - vfx) ) + im*imag( vfx1 - vfx ) ) / (2*epsilon)
-            x1[i] -= epsilon
-            x[i] += epsilon
+            @. J[:,i] = ( real( (vfx - vfx1) ) + im*imag(vfx - vfx1) ) / (2*epsilon)
+            x1[i] = x1_save
+            x[i] = x_save
         end
     else
         fdtype_error(Val{:Complex})
