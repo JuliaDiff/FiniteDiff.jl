6ca20f820cae4c988ca391730f17fafd196d9da0
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2017-11-21 03:01:30 -0800

    warn when cache is ignored

--- src/jacobians.jl
+++ src/jacobians.jl
@@ -4,10 +4,16 @@ struct JacobianCache{CacheType,CacheType2,CacheType3,fdtype,RealOrComplex}
     fx1::CacheType3
 end
 
-function JacobianCache(x1,fx,fx1,fdtype::DataType=Val{:central},
+function JacobianCache(x1,fx,_fx1,fdtype::DataType=Val{:central},
                        RealOrComplex::DataType =
                        fdtype==Val{:complex} ? Val{:Real} : eltype(x1) <: Complex ?
                        Val{:Complex} : Val{:Real})
+    if fdtype == Val{:complex} && fx1 != nothing
+        warn("fx1 cache is ignored when fdtype == Val{:complex}.")
+        fx1 = nothing
+    else
+        fx1 = _fx1
+    end
     JacobianCache{typeof(x1),typeof(fx),typeof(fx1),
                   fdtype,RealOrComplex}(x1,fx,fx1)
 end
