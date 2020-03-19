66bcf264bb32bd965b71f1cdb8c35c8e4c65ed90
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-03-19 11:34:16 -0400

    use an internal vec

--- src/FiniteDiff.jl
+++ src/FiniteDiff.jl
@@ -5,11 +5,16 @@ using LinearAlgebra, SparseArrays, StaticArrays, ArrayInterface, Requires
 
 import Base: resize!
 
+_vec(x) = vec(x)
+_vec(x::Number) = x
+
 include("iteration_utils.jl")
 include("epsilons.jl")
 include("derivatives.jl")
 include("gradients.jl")
 include("jacobians.jl")
 include("hessians.jl")
+
+
 
 end # module
